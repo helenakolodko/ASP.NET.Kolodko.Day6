2a0a4b20c7618710f50bb0830aa077f3fd8280b4
helenakolodko <helena.kolodko@gmail.com>
2015-07-24 21:01:18 +0300

    Added refactored IntMartixSorter.

---
+++ Task1.Library/IntMatrixSorter_Delegates.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Task1.Library
+{
+    public static class IntMatrixSorter_Delegates
+    {
+        public static void SortRows(int[][] array, IComparer comparer)
+        {
+            SortRows(array, (a, b) => comparer.Compare(a, b));
+        }
+
+
+        public static void SortRows(int[][] array, Comparison<int[]> comparison)
+        {
+            int i = 1;
+            bool found = true;
+
+            while (i < array.Length && found)
+            {
+                found = false;
+                for (int j = array.Length - 1; j >= i; j--)
+                {
+                    if (comparison(array[j - 1], array[j]) > 0)
+                    {
+                        SwapElements(array, j, j - 1);
+                    }
+                    found = true;
+                }
+                i++;
+            }
+        }
+
+        private static void SwapElements(int[][] array, int i, int j)
+        {
+            int[] temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+    }
+}
---
+++ Task1.Library/IntMatrixSorter_Interfaces.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+
+namespace Task1.Library
+{
+    public static class IntMatrixSorter_Interfaces
+    {
+        public static void SortRows(int[][] array, Comparison<int[]> comparison)
+        {
+            SortRows(array, new DelegateAdapter(comparison));
+        }
+        public static void SortRows(int[][] array, IComparer comparer)
+        {
+            int i = 1;
+            bool found = true;
+
+            while (i < array.Length && found)
+            {
+                found = false;
+                for (int j = array.Length - 1; j >= i; j--)
+                {
+                    if (comparer.Compare(array[j - 1], array[j]) > 0)
+                    {
+                        SwapElements(array, j, j - 1);
+                    }
+                    found = true;
+                }
+                i++;
+            }
+        }
+
+        private static void SwapElements(int[][] array, int i, int j)
+        {
+            int[] temp = array[j];
+            array[j] = array[i];
+            array[i] = temp;
+        }
+
+        private class DelegateAdapter: IComparer
+        {
+            private Comparison<int[]> comparison;
+            public DelegateAdapter(Comparison<int[]> comparison)
+            {
+                if (ReferenceEquals(comparison, null))
+                    throw new ArgumentNullException();
+                this.comparison = comparison;
+            }
+            public int Compare(object x, object y)
+            {
+                return comparison((int[]) x,(int[]) y);
+            }
+        }
+    }
+}
